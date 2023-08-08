1d8b657677a1488f1bb90bb9a0847bf462c2874c
GabrielLisandro <glk.05@outlook.com>
2023-08-08 16:17:09 -0300

    perro.java
    Agregue un perro

---
+++ PruebaGitHub/src/pruebagithub/Perro.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebagithub;
+
+/**
+ *
+ * @author glk05
+ */
+public class Perro {
+    String raza;
+    int edad;
+
+    public Perro(String raza, int edad) {
+        this.raza = raza;
+        this.edad = edad;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    
+}
